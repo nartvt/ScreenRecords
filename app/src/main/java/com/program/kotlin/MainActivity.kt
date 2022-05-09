3709c6065e32fb418af5673a68946dababd07e01
Tai <vantai@lozi.vn>
2022-05-09 22:30:46 +0700

    request permission

--- app/src/main/java/com/program/kotlin/MainActivity.kt
+++ app/src/main/java/com/program/kotlin/MainActivity.kt
@@ -8,15 +8,17 @@ import android.os.Bundle
 import android.util.DisplayMetrics
 import android.util.Log
 import android.Manifest
+import android.app.Activity
 import android.view.View
 import android.widget.Chronometer
 import android.widget.TextView
 import android.widget.ToggleButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
-    private var toggleButton:ToggleButton?=null
     private var chronometer:Chronometer?=null
     private var readText:TextView?=null
 
@@ -30,30 +32,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         chronometer=findViewById(R.id.cmTimer)
         readText=findViewById(R.id.tvRecord)
-        toggleButton=findViewById(R.id.tbSwitch)
+        val toggleButton=findViewById(R.id.tbSwitch)
 
         val displayMetrics = DisplayMetrics()
         screenDensity = displayMetrics.densityDpi
         mediaRecorder = MediaRecorder()
         mediaProjectionManager = getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
 
-        toggleButton.setOnClickListener(View.OnClickListener {
-            setupPermissions()
-        })
+        toggleButton.setOnClickListener(
+            setupPermissions(toggleButton)
+        )
     }
 
-    private fun setupPermissions() {
+    private fun setupPermissions(toggleButton: ToggleButton) {
         val permissionWriteStorage = ContextCompat.checkSelfPermission(this,
             Manifest.permission.WRITE_EXTERNAL_STORAGE)
 
         val permission = ContextCompat.checkSelfPermission(this,
             Manifest.permission.RECORD_AUDIO)
-        if (permissionWriteStorage == PackageManager.PERMISSION_GRANTED && permission == PackageManager.PERMISSION_GRANTED){
-
-        }
-
-        if (permission != PackageManager.PERMISSION_GRANTED) {
+        if (permissionWriteStorage != PackageManager.PERMISSION_GRANTED ||  permission != PackageManager.PERMISSION_GRANTED){
             Log.i("Permission denine","Permission to record denied")
+            ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.RECORD_AUDIO)
+            toggleButton.isChecked = false
+            Snackbar.make(findViewById(R.id.content),R.string.permission_text,Snackbar.LENGTH_INDEFINITE).setAction("ENABLE",View.OnClickListener {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.RECORD_AUDIO
+                    ), // TODO
+            // )
+            })
         }
     }
 }
